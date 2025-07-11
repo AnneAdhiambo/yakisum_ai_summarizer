353ef6a9c952c2622d39d8b6399c0fcc0787f0a1
Evans <snaveqiptoo@gmail.com>
2025-07-11 17:02:53 +0300

    Use logo.svg as Yakisum app logo in header

--- components/header.tsx
+++ components/header.tsx
@@ -26,7 +26,7 @@ export function Header({ onMenuClick, activeSection }: HeaderProps) {
         </Button>
         <div className="flex items-center gap-2 sm:gap-3">
           <div className="w-8 h-8 sm:w-10 sm:h-10 bg-orange-500 rounded-lg flex items-center justify-center">
-            <FaRobot className="text-white w-5 h-5 sm:w-6 sm:h-6" />
+            <img src="/logo.svg" alt="Yakisum Logo" className="w-5 h-5 sm:w-6 sm:h-6" />
           </div>
           <h1 className="font-semibold text-gray-900 text-sm sm:text-base lg:text-lg">
             {sectionTitles[activeSection]}
